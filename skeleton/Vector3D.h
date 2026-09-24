46691477e82aa47365e488cc4b41f12f55787f3e
Ricardo Luy <130751044+rluy02@users.noreply.github.com>
2026-09-24 22:46:39 +0200

    simplificacion del Vector3D y wrapper
    se habian agregado cosas innecesarias y ademas, los atributos tenian que ser publicos.
    Ya esta la conversion

--- skeleton/Vector3D.h
+++ skeleton/Vector3D.h
@@ -1,85 +1,23 @@
 #pragma once
-#include "PxPhysicsAPI.h";
+#include "PxPhysicsAPI.h"
 #include <cassert>
 #include <cmath>
 
 class Vector3D {
 public:
-	Vector3D::Vector3D() : _x(0), _y(0), _z(0) {}
-	Vector3D::Vector3D(float x, float y, float z) : _x(x), _y(y), _z(z) {}
-	Vector3D(const Vector3D& v) noexcept : _x(v.getX()), _y(v.getY()), _z(v.getZ()) {}
-	Vector3D(Vector3D&& v) noexcept : _x(v.getX()), _y(v.getY()), _z(v.getZ()) {}
-	Vector3D::Vector3D(physx::PxVec3 vec) : _x(vec.x), _y(vec.y), _z(vec.z) {}
-	~Vector3D() {}
+	Vector3D() : x(0), y(0), z(0) {}
+	Vector3D(float x, float y, float z) : x(x), y(y), z(z) {}
+	Vector3D(const physx::PxVec3& vec) : x(vec.x), y(vec.y), z(vec.z) {}
 
-	inline float getX() const { return _x; }
-
-	inline float getY() const { return _y; }
-
-	inline float getZ() const { return _z; }
-
-	// various setters
-	inline void setX(float x) { _x = x; }
-	inline void setY(float y) { _y = y; }
-	inline void setZ(float z) { _z = z; }
-
-	inline void set(float x, float y, float z)
-	{
-		_x = x;
-		_y = y;
-		_z = z;
-	}
-
-	inline void set(const Vector3D& v)
-	{
-		_x = v._x;
-		_y = v._y;
-		_z = v._z;
-	}
+	float x, y, z; //los piden publicos explicitamente
 
 	// copy assignment
 	inline Vector3D& operator=(const Vector3D& v) noexcept
 	{
-		_x = v._x;
-		_y = v._y;
-		_z = v._z;
+		x = v.x;
+		y = v.y;
+		z = v.z;
 		return *this;
-	}
-
-	// v[0] is the first coordinate and v[1] is the second
-	inline float& operator[](int i) noexcept
-	{
-		assert(i == 0 || i == 1 || i == 2);
-		switch (i)
-		{
-		case 0:
-			return _x;
-			break;
-		case 1:
-			return _y;
-			break;
-		case 2:
-			return _z;
-			break;
-		}
-	}
-
-	// v[0] is the first coordinate and v[1] is the second
-	inline const float& operator[](int i) const noexcept
-	{
-		assert(i == 0 || i == 1 || i == 2);
-		switch (i)
-		{
-		case 0:
-			return _x;
-			break;
-		case 1:
-			return _y;
-			break;
-		case 2:
-			return _z;
-			break;
-		}
 	}
 
 	// ** various operations
@@ -88,36 +26,38 @@ public:
 	// vector addition
 	inline Vector3D operator+(const Vector3D& v) const
 	{
-		return Vector3D(_x + v._x, _y + v._y, _z + v._z);
+		return Vector3D(x + v.x, y + v.y, z + v.z);
 	}
 
+	inline Vector3D& operator+=(const Vector3D& v)
+	{
+		x += v.x;
+		y += v.y;
+		z += v.z;
+		return *this;
+	}
+	// subtraction 
 	inline Vector3D operator-(const Vector3D& v) const
 	{
-		return Vector3D(_x - v._x, _y - v._y, _z - v._z);
+		return Vector3D(x - v.x, y - v.y, z - v.z);
 	}
 
 	// mult by constant
 	inline Vector3D operator*(float d) const
 	{
-		return Vector3D(_x * d, _y * d, _z * d);
+		return Vector3D(x * d, y * d, z * d);
 	}
-	//mult (escalar)
-	inline float operator*(const Vector3D& d) const
-	{
-		return d._x * _x + d._y * _y + d._z * _z;
-	}
-
 
 	// division by constant (scaling)
 	inline Vector3D operator/(float d) const
 	{
-		return Vector3D(_x / d, _y / d, _z / d);
+		return Vector3D(x / d, y / d, z / d);
 	}
 
 	// equal
 	inline bool operator==(const Vector3D& v) const
 	{
-		return _x == v._x && _y == v._y && _z == v._z;
+		return x == v.x && y == v.y && z == v.z;
 	}
 
 	// not equal
@@ -126,37 +66,47 @@ public:
 		return !(*this == v);
 	}
 
+	// ** logic
+	//
+
 	// length of the vector
 	inline float magnitude() const
 	{
-		return sqrtf(powf(_x, 2) + powf(_y, 2) + powf(_z, 2));
+		return sqrtf(powf(x, 2) + powf(y, 2) + powf(z, 2));
 	}
 
 	// vector in the same direction of length 1
 	inline Vector3D normalize() const
 	{
 		float m = magnitude();
-		assert(m > 0.000001f); // extra para saber si se divide entre 0
+		assert(m > 0.000001f, "An operation is attempting to divide by zero."); // extra para saber si se divide entre 0
 		if (m <= 0.000001f)
 			return Vector3D(); // empty
-		return *this / magnitude();
+		return *this / m;
 	}
 	// It returns a scalar value indicating vector alignment : positive(same direction,<90), negative(opposite
 	// directions,>90), or zero(orthogonal,90) ABC=|A||B||C|cos(angle)
 	inline float dot(const Vector3D& other) const
 	{
-		return _x * other._x + _y * other._y + _z * other._z;
+		return x * other.x + y * other.y + z * other.z;
 	}
 
-	//producto vectorial
+	// (producto vectorial)
 	Vector3D cross(const Vector3D& v) const {
 		return Vector3D(
-			_y * v._z - _z * v._y,
-			-(_x * v._z - _z * v._x),
-			_x * v._y - _y * v._x
+			y * v.z - z * v.y,
+			-(x * v.z - z * v.x),
+			x * v.y - y * v.x
 		);
 	}
-private:
-	float _x, _y, _z;
+
+	// ** conversions
+	//
+	
+	// implicit 
+	operator physx::PxVec3() const
+	{
+		return physx::PxVec3(x, y, z);
+	}
 
 };
