b67bd1b210cf482eb56b197eba254a6ba721bb9e
Ricardo Luy <130751044+rluy02@users.noreply.github.com>
2026-09-24 23:01:50 +0200

    Primera escena implementada

--- skeleton/SceneManager.cpp
+++ skeleton/SceneManager.cpp
@@ -51,6 +51,10 @@ void SceneManager::keyPress(unsigned char key, const physx::PxTransform& cameraT
         changeScene("EscenaVacia");
         return; // Consumimos el evento para que no interfiera con la escena
     }
+    if (key == '0') {
+        changeScene("P0S_Scene");
+        return; // Consumimos el evento para que no interfiera con la escena
+    }
 
     // Si no es una tecla de navegación global, se la pasamos a la escena activa
     if (m_currentScene != nullptr) {
--- skeleton/main.cpp
+++ skeleton/main.cpp
@@ -22,6 +22,7 @@
 // Para las escenas del curso, se incluyen los headers de las prácticas y la escena vacía
 #include "SceneManager.h"
 #include "EmptyScene.h"
+#include "P0S_Scene.h"
 
 #include <foundation/PxSimpleTypes.h>
 #include <PxPhysicsVersion.h> // <- Macros for PhysX version checking
@@ -98,6 +99,7 @@ void initPhysics(bool interactive)
 	gScene = gPhysics->createScene(sceneDesc);
 	// Registrar las prácticas/escenas del curso
 	SceneManager::instance().registerScene<EmptyScene>("EscenaVacia");
+	SceneManager::instance().registerScene<P0S_Scene>("P0S_Scene");
 	
 	// Cargar la escena inicial
 	SceneManager::instance().changeScene("EscenaVacia");
